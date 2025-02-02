2893cbbbc38b02e505d008d1358d4b970e792ad7
serenity-scents <cherrymaeroxas96@gmail.com>
2025-02-02 16:15:16 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
-
-window.onload=function(){
+function playWelcomeMessage() {
     speak("./media/welcome.mp3");
     setTimeout(() => {
         speak("./media/tagline.mp3");
     },2500)
-
 }
+
+// Play the welcome message when the page loads
+window.addEventListener('DOMContentLoaded', playWelcomeMessage);
