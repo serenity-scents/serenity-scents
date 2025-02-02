40fdf13d4149136e6a04582748e04cdb902a7e16
serenity-scents <cherrymaeroxas96@gmail.com>
2025-02-02 16:21:45 +0800

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+
+// window.onload=function(){
+//     speak("./media/welcome.mp3");
+//     setTimeout(() => {
+//         speak("./media/tagline.mp3");
+//     },2500)
+
+// }
 function playWelcomeMessage() {
     speak("./media/welcome.mp3");
     setTimeout(() => {
